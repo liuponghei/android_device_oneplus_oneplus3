235acd96fec89f2e1e6bd29462b159718c9a9adb
liuponghei <liuponghei@users.noreply.github.com>
2018-07-15 01:14:42 +0800

    Update hidl.mk

--- hidl.mk
+++ hidl.mk
@@ -59,7 +59,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service.oneplus3
+    android.hardware.power@1.0-service.Z01K
 
 # health
 PRODUCT_PACKAGES += \
